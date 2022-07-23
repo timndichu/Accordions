9e7740aadfcddea0a617e419f99fc3ff4f828fc1
Timothy Ndichu <44669570+timndichu@users.noreply.github.com>
2022-07-23 14:00:12 +0300

    File restructure and readme update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-const accordion = document.getElementsByClassName('container');
+// getElementsByClassName: Returns a HTMLCollection of the elements in the object on which the method was invoked (a document or an element) that have all the classes given by classNames.
+const accordion = document.getElementsByClassName("container");
 
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
+// attach a click listener to each element within the container class
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    /**toggle active class
+     * (if element has active class onclick: remove active class)
+     * (else element doesnt have active class onclick: add active class)
+     */
+    this.classList.toggle("active");
+  });
 }
